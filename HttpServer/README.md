2c553f545ecc3f96a4f7f1def67d879170e3bcfd
Colbot Zhang <colbot.zhang@hotmail.com>
2017-08-19 19:42:29 +0800

    Add httpserver

---
+++ HttpServer/README.md
@@ -0,0 +1,19 @@
+支持文件上传的HTTPServer
+=====
+
+## 默认
+Python 默认的 **SimpleHTTPServer** 用起来很方便，偶尔需要传送文件时直接从 Python 启动一个 HTTPServer 即可，无需其他工具。启动命令为
+```python
+python -m SimpleHTTPServer [port]
+```
+一般 *[port]* 选择 *1024* 以后的端口，防止权限不足，默认为 *8000*。  
+
+Python3 默认为 **http.server** ，启动命令为
+```python
+python3 -m http.server [port]
+```
+
+## 改进
+Python自带的 HTTP 服务器只有页面显示和文件下载功能，不支持文件上传，使用起来多有不便，这里在默认 SimpleHTTPServer 的基础上增加文件上传功能。  
+实现方式为重载 `SimpleHTTPRequestHandler` 的 `do_POST` 和 `list_directory` 方法。其中在 `do_POST` 方法中实现文件保存，在 `list_directory` 方法中增加一个 POST 文件的表单。  
+具体实现方式见 [httpServer.py](httpServer.py)
---
+++ HttpServer/httpServer.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+import os
+import html
+import urllib
+import cgi
+import sys
+import mimetypes
+import argparse
+from io import BytesIO
+from http import HTTPStatus
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
+
+__version__ = "0.7"
+__all__ = ["UploadHTTPHandler"]
+
+class UploadHTTPHandler(SimpleHTTPRequestHandler):
+
+    server_version = "SimpleHTTP/" + __version__
+
+    def do_POST(self):
+        """Serve a POST request."""
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+        filename = form['file'].filename
+        path = self.translate_path(self.path)
+        filepath = os.path.join(path, filename)
+        if os.path.exists(filepath):
+            self.log_error('File %s exist!', filename)
+            filepath += '.new'
+
+        with open(filepath, 'wb') as f:
+            f.write(form['file'].value)
+        super().do_GET()
+
+    def list_directory(self, path):
+
+        try:
+            list = os.listdir(path)
+        except OSError:
+            self.send_error(
+                HTTPStatus.NOT_FOUND,
+                "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        r = []
+        try:
+            displaypath = urllib.parse.unquote(self.path,
+                                               errors='surrogatepass')
+        except UnicodeDecodeError:
+            displaypath = urllib.parse.unquote(path)
+        displaypath = html.escape(displaypath)
+        enc = sys.getfilesystemencoding()
+        title = 'Directory listing for %s' % displaypath
+        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
+                 '"http://www.w3.org/TR/html4/strict.dtd">')
+        r.append('<html>\n<head>')
+        r.append('<meta http-equiv="Content-Type" '
+                 'content="text/html; charset=%s">' % enc)
+        r.append('<title>%s</title>\n</head>' % title)
+        r.append('<body>\n<h1>%s</h1>' % title)
+        r.append('''<form action="" enctype="multipart/form-data" method="post">\n
+                    <input name="file" type="file" />
+                    <input value="upload" type="submit" />
+                </form>''')
+        r.append('<hr>\n<ul>')
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            r.append('<li><a href="%s">%s</a></li>'
+                    % (urllib.parse.quote(linkname,
+                                          errors='surrogatepass'),
+                       html.escape(displayname)))
+        r.append('</ul>\n<hr>\n</body>\n</html>\n')
+        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
+        f = BytesIO()
+        f.write(encoded)
+        f.seek(0)
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-Length", str(len(encoded)))
+        self.end_headers()
+        return f
+
+    if not mimetypes.inited:
+        mimetypes.init() # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream', # Default
+        '.py': 'text/plain',
+        '.c': 'text/plain',
+        '.h': 'text/plain',
+        })
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address '
+                             '[default: all interfaces]')
+    parser.add_argument('port', action='store',
+                        default=8000, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 8000]')
+    args = parser.parse_args()
+    server_address = (args.bind, args.port)
+
+    UploadHTTPHandler.protocol_version = "HTTP/1.0"
+    httpd = HTTPServer(server_address, UploadHTTPHandler)
+
+    sa = httpd.socket.getsockname()
+    print("Serving HTTP on", sa[0], "port", sa[1], "...")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        httpd.server_close()
+        sys.exit(0)
